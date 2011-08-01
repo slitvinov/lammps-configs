2be15528752e3e76cd75fc776421c1b4d2f83fd0
Litvinov Sergey <slitvinov@gmail.com>
2011-08-01 14:36:25 +0200

    Can run with scretching

--- dpd-with-wall-polymers/run.sh
+++ dpd-with-wall-polymers/run.sh
@@ -3,5 +3,6 @@
 set -e
 set -u
 
-/scratch/work/lammps-ro/tools/chain2/src/chain2 -i in.generator -o chain.in -d def.chain  -echo both --verbose
-../../src/lmp_linux -in in.fedosov
+mpirun=/scratch/prefix-mpich/bin/mpirun
+/scratch/work/lammps-ro/tools/chain2/src/chain2 -i in.generator -o chain.in -d def.chain  -echo both --verbose --extra-bond
+${mpirun} -np 1 ../../src/lmp_linux -in in.fedosov
