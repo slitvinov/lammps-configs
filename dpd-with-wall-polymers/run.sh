cd35114faa3436d0057716b60034b0b8ce6a0280
Litvinov Sergey <slitvinov@gmail.com>
2011-08-01 20:14:52 +0200

    Making a setup for straching simulations

--- dpd-with-wall-polymers/run.sh
+++ dpd-with-wall-polymers/run.sh
@@ -5,4 +5,4 @@ set -u
 
 mpirun=/scratch/prefix-mpich/bin/mpirun
 /scratch/work/lammps-ro/tools/chain2/src/chain2 -i in.generator -o chain.in -d def.chain  -echo both --verbose --extra-bond
-${mpirun} -np 1 ../../src/lmp_linux -in in.fedosov
+${mpirun} -np 8 ../../src/lmp_linux -in in.fedosov
---
+++ dpd-with-wall-polymers/spat.awk
@@ -0,0 +1,21 @@
+#! /usr/bin/awk -f 
+    BEGIN {
+  nsnap =  0
+    }
+
+NF==4{
+  if (n==1) nspan++
+  n++
+  x[n]=$2
+  s[n]+=$4
+}
+
+NF==2{
+  n=0
+}
+
+END {
+  for (i=1; i<n+1; i++) {
+    print x[i], s[i]/nspan
+  }
+}
