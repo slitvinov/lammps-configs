6a06bcfbbb2a33b5bb32afbd4de09fe8fc96f459
Litvinov Sergey <slitvinov@gmail.com>
2012-02-02 19:57:35 +0100

    Remove tmp directories

--- dpd-old-inf15/update.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-gnuplot vel.gp
-cp vel.png ~/Dropbox/Public/dpd/
-
-cd /scratch/work/lammps-ro/lammps-configs/dpd-old/
-gnuplot vel.gp
-cp vel.png ~/Dropbox/Public/dpd/vel-inf.png
-cp stress.png ~/Dropbox/Public/dpd/stress-inf.png
--- ext-lmp-try6/saver.gp
+++
@@ -1,8 +0,0 @@
-set term push
-set terminal png
-set output sprintf("%s.png", "$0")
-replot
-set output
-
-set term pop
-replot
