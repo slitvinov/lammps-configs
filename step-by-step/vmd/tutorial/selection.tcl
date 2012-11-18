e5f37583dc3e3f565d731e63324b95839adab3cd
Sergey Litvinovn <slitvinov@gmail.com>
2012-11-18 11:01:29 +0100

    Extended commands ins selection

--- step-by-step/vmd/tutorial/selection.tcl
+++ step-by-step/vmd/tutorial/selection.tcl
@@ -1,18 +1,22 @@
 # Run with
-# vmd -dispdev text -psf data.psf -dcd data.dcd -e selection.tcl 
+# vmd -dispdev text -psf data.psf -dcd data.dcd -e selection.tcl
+# To use vmd in emacs as a tcl shell
+# C-u run-tcl ./r.sh
+log ~/vmd.tcl
 
 set sel [atomselect top all]
-# number of atoms in selection
+# number of atoms in the selection
 $sel num
 
-# list of inexes
+# list of indexes
 $sel list
 
-# shoe frame frame
+# show frame frame
 $sel frame
 
 # set frame for the selection
 $sel frame now
+
 # and show it
 $sel frame
 
@@ -36,27 +40,33 @@ $sel set y 10
 # - setbonds: Set the bonds for the atoms in the selection; the second
 #   argument should be a list of bondlists, one bondlist for each
 #   selected atom.
-
-
 puts "======bonds===="
 set blist [$sel getbonds]
 
-# bonded to atom 1
+# atom with index 0 is bonded to atom 1
 lindex ${blist} 0
 
-# bonded to atom 0 and 2
+# atom with index 1 is bonded to atoms 0 and 2
 lindex ${blist} 1
 
-# bonded to atom 1 and 3
+# atom with index 2 is bonded to atoms 1 and 3
 lindex ${blist} 2
 
-# radius of gyration
+# radius of gyration for different frames
+$sel frame first
 measure rgyr ${sel}
-
+$sel frame now
+measure rgyr ${sel}
 puts "======bonds===="
-exit
 
 # to see the chain
-#pbc box
-#scale to 0.04
-#rotate x to -90
+pbc box
+scale to 0.04
+rotate x to -90
+
+# print a summary of the moleculs
+mol list
+mol list 0
+
+mol addrep 0
+mol modstyle 1 0 Points 10.0
