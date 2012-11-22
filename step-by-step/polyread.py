1d56ad4e97caa8a567f1ad8e626001a672482dd1
Litvinov Sergey <slitvinov@gmail.com>
2012-11-22 15:36:03 +0100

    Add a polyread file

---
+++ step-by-step/polyread.py
@@ -0,0 +1,15 @@
+# work with data file
+
+
+import data
+
+# export a data file
+d = data.data("polymer.data")
+
+# add a new bond
+d.sections["Bonds"] = ['1 1 1 3\n', '1 1 1 4\n']
+d.headers["bonds"] = 2
+
+# write a data file
+d.write("out.dat")
+
