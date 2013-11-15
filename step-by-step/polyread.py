160aea683bdc199bd2ca4e057ee06bfdebc96378
Litvinov Sergey <slitvinov@gmail.com>
2013-11-15 22:51:43 +0100

    Small fixes

--- step-by-step/polyread.py
+++ step-by-step/polyread.py
@@ -1,7 +1,10 @@
 # work with data file
+# data.py from Pizza should be on PYTHONPATH
+# PYTHONPATH=/scratch/work/Pizza.py/src/ python polyread.py
 import data
 
 # export a data file
+print "reading polyr.data"
 d = data.data("polymer.data")
 
 # add a new bond
@@ -9,5 +12,6 @@ d.sections["Bonds"] = ['1 1 1 3\n', '1 1 1 4\n']
 d.headers["bonds"] = 2
 
 # write a data file
+print "writing out.dat"
 d.write("out.dat")
 
