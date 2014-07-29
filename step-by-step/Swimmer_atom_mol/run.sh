e0d81cd3f8ed5d945a57f2133aeb1fc23ad6a703
Sergey Litvinov <slitvinov@gmail.com>
2014-07-29 19:45:08 +0200

    Change parameters for the swimmer simulations

--- step-by-step/Swimmer_atom_mol/run.sh
+++ step-by-step/Swimmer_atom_mol/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# http://stackoverflow.com/a/6930607
+# make the script fail on error
+set -e
+set -u
+
 # the number of bonds: Nb
-#Na= Number of Angles
+# the number of aggles: Na (for a linear chain Nb-1)
 
 Nb=5
 Na=4 # define Na= Nb-1
 
-./lmp_linux -var Nb ${Nb} Na ${Na}   -in in.geninit
+./lmp_linux -var Nb ${Nb} -var Na ${Na}   -in in.geninit
 awk -v Nb=${Nb} -v Na=${Na} -f addpolymer.awk data.grid > data.polymer
 
-./lmp_linux -in in.run
+./lmp_linux -var Nb ${Nb} -in in.run
