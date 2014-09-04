e303ef2788c2bcbaab9a189aa12c81a360a0834f
Tiago Faria <tiago_gfaria@hotmail.com>
2014-09-04 16:47:34 +0200

    Using templates to generate addswimmer.awk

--- step-by-step/Swimmer_atom_transport/run.sh
+++ step-by-step/Swimmer_atom_transport/run.sh
@@ -17,6 +17,7 @@ mpirun=~/prefix-mpi/bin/mpirun
 ${lmp} -var sw_length ${sw_length} -in in.geninit
 
 # create bonds for the swimmer
+awk -f ../scripts/template_eng.awk  ../scripts/addswimmer.awk > addswimmer.awk
 awk -v sw_length=${sw_length} -f addswimmer.awk data.grid > data.bond_nn
 
 # count the number of bonds
@@ -25,5 +26,5 @@ awk -f count_bonds.awk data.bond_nn data.bond_nn > data.bond
 # make all atoms of the swimmer of the type `new_type'
 awk -v new_type=2 -f change_type_of_bonded.awk  data.bond data.bond > data.polymer
 
-${mpirun} -np 4 ${lmp} \
-    -var sw_length ${sw_length} -in in.run
+#${mpirun} -np 4 ${lmp} \
+#    -var sw_length ${sw_length} -in in.run
